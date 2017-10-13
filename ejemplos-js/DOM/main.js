982d34fd7ca4ede696c469a538ed6b3e08afdd53
abrahamgalarza <abraham_uaslp@outlook.es>
2017-10-13 03:52:50 -0500

    JavaScript

--- ejemplos-js/DOM/main.js
+++ ejemplos-js/DOM/main.js
@@ -1,48 +1,69 @@
-var cajas = document.getElementsByName('div');
-
 var btnAgregar = document.getElementById("btnAgregar");
-/**Event Handler */
-/*btnAgregar.onclick = agregaCaja;*/
 
-/*Event Listener */
-btnAgregar.addEventListener('click',function(){
-    var entrada = document.getElementById('entrada');
-    var contenido = entrada.value;
-    if(contenido != '')
-        agregaCaja(contenido);
-});
-//document.addEventListener('click', agregaCaja);
-//document.addEventListener('click',prueba);
-
-
-var padre = document.getElementById("contenedor");
-//Delegacion de eventos
-padre.addEventListener('click', function(evento){
+var padre = document.getElementById('contenedor');
+padre.addEventListener('click', function(evento)
+{
     var hijo = evento.target;
-    if(hijo != padre){//Validar que no se hizo click en el contendor
+    
+    if(hijo!=padre)
+    {
         this.removeChild(hijo);
     }
-});
+}
+);
 /*
-var cajas  = document.getElementsByClassName("caja");
-for(var i = 0; i < cajas.length; i++)
+var cajas = document.getElementsByClassName('caja');
+for(var i=0;i<cajas.length;i++)
 {
-    cajas[i].addEventListener('click',function() {
+    cajas[i].addEventListener('click',function()
+    {
         padre.removeChild(this);
     });
 }*/
+//cajas[0].addEventListener('click',function()
 
+
+/*
+btnAgregar.onclick = agregaCaja;
+btnAgregar.onclick = prueba;
+btnAgregar.onclick = function()
+{
+    window.alert('Ejecutando funcion anonima');
+}*/
+/* envent listener*/
+btnAgregar.addEventListener('click', function(){
+    var entrada = document.getElementById('entrada');
+    var contenido = entrada.value;
+    if(contenido!="")
+    {
+        agregaCaja(contenido);
+    }
+    
+}
+);
+//document.addEventListener('click',agregaCaja);
+/*document.addEventListener('click',prueba);
+document.addEventListener('click', function() {
+    window.alert('funcion anonima');
+}
+);*/
 function agregaCaja(texto)
 {
-    var nuevo = document.createElement('div');
-    var texto = document.createTextNode(texto);
-    nuevo.appendChild(texto);
-    nuevo.setAttribute("class", "caja");
-    var padre = document.getElementById('body');
-    padre[0].appendChild(nuevo);
+    window.alert('agregando');
+var nuevo = document.createElement('div');
+var texto = document.createTextNode(texto);
+nuevo.appendChild(texto);
+nuevo.setAttribute("class","caja");
+var padre = document.getElementsById('contenedor');
+padre.appendChild(nuevo);
+
 }
 
 function prueba()
 {
-    window.alert("Hola");
+    window.alert('hola');
 }
+
+
+
+//agregaCaja();
