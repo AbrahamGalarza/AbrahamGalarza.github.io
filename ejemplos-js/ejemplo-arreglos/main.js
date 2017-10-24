df5f1f43d9a06a6d15c1a9d84a1b1ee62972fd4e
abrahamgalarza <abraham_uaslp@outlook.es>
2017-10-24 03:01:47 -0500

    Tabla

--- ejemplos-js/ejemplo-arreglos/main.js
+++ ejemplos-js/ejemplo-arreglos/main.js
@@ -1,4 +1,3 @@
-
 var materias = [
     { nombre: "Tecnologias Web", creditos: 8, optativa: true },
     { nombre: "Pensamiento Computacional", creditos: 10, optativa: false },
@@ -9,11 +8,41 @@ muestraMaterias();
 
 
 function muestraMaterias(){
-    var tabla = document.createElement('table');
-    tabla.appendChild(crearEncabezado());
-    document.body.appendChild(tabla);
+    var tabla = document.createElement('table');//Crea elemento tabla
+    tabla.setAttribute('id','tabla'); //Asigna un ID
+    tabla.appendChild(crearEncabezado());//Agrega nodo
+    document.body.appendChild(tabla); //Agrega nodo
 
-    console.log('for-normalito');
+    //Recorrer Arreglo con forEach
+
+    materias.forEach(function (mat){
+
+        var renglon = document.createElement('tr');
+
+        ///Columna Nombre
+        var columna1 = document.createElement('td');
+        var nombre = document.createTextNode(mat.nombre);
+        columna1.appendChild(nombre);
+
+        //Columna Creditos
+        var columna2 = document.createElement('td');
+        var creditos = document.createTextNode(mat.creditos);
+        columna2.appendChild(creditos);
+
+        //Columna Optativa
+        var columna3 = document.createElement('td');
+        var optativa = document.createTextNode(mat.optativa);
+        columna3.appendChild(optativa);
+
+        renglon.appendChild(columna1); //Agrega Columna 1 a renglon nombre
+        renglon.appendChild(columna2); //Agrega Columna 2 a renglon 
+        renglon.appendChild(columna3); //Agrega Columna 3 a renglon
+
+        tabla.appendChild(renglon);
+
+    });
+
+    /*console.log('for-normalito');
     for(var i =0; i < materias.length; i++){
         console.log('Nombre: ' + materias[i].nombre);
         console.log('Creditos: ' + materias[i].creditos);
@@ -24,7 +53,7 @@ function muestraMaterias(){
         console.log('Nombre: '+ mat.nombre);
         console.log('Creditos: ' + mat.creditos);
         console.log('Optativa: '+ mat.optativa ? 'Sí' : 'No');
-    });
+    });*/
 }
 
 function crearEncabezado(){
@@ -48,21 +77,3 @@ function crearEncabezado(){
     
         return encabezado;
 }
-
-
-function crearFila(){
-
-    var fila = document.createElement('tr');
-
-    var nom = document.createElement('td');
-    var textNom = document.createTextNode('Nombre');
-    nom.appendChild(textNom);
-
-    var cred = document.createElement('td');
-    var textCred = document.createTextNode('');
-    cred.appendChild(textCred);
-
-    var opt = document.createElement('td');
-    var textOpt = document.createElement('td');
-    opt.appendChild(textOpt);
-}  
